682a7bc488b515d9a92683d858199bd4e6af5a9d
Namrata <singhnamrata683@gmail.com>
2023-03-04 21:37:48 -0800

    message

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
    path('api/', include('rest_framework.urls')),
     path('',include('figma.urls')),
+    path('newblog/', include('newblog.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
